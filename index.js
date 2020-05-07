1bdbc7f3818be4203bbf6bd52b680543537ca102
unknown <locbgiang@gmail.com>
2020-05-07 12:40:26 -0700

    ready for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require('./server');
-
-server.listen(5000, () => {
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
     console.log('\n Server Running on http://localhost:5000 \n');
 });
